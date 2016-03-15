84f61ee2b7b23ea95f9832bb64b62918012665d9
cp <cesar.passoni@gmail.com>
2016-03-15 13:20:36 -0400

    basic bash profile and basic installation script.

---
+++ install_Mac.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install ack
+brew install fish
+brew install imagemagick
+brew install libtool
+brew install wget
+brew install bash-completion
+brew install freetype
+brew install jpeg
+brew install node
+brew install x264
+brew install brew-cask
+brew install git
+brew install lame
+brew install openssl
+brew install xvid
+brew install coreutils
+brew install grc
+brew install libpng
+brew install spark
+brew install xz
+brew install ffmpeg
+brew install htop
+brew install libtiff
+brew install ssh-copy-id
+brew install z
+
+
+
+brew cask install alfred
+brew cask install github-desktop
+brew cask install intellij-idea
+brew cask install sourcetree
+brew cask install atom
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install sublime-text
+brew cask install firefox
+brew cask install hipchat
+brew cask install skype
+brew cask install vlc
+
